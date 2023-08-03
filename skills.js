6e6a37bcac955a3a36e81f3b1db2b94654a3a972
HarleyHa <121558266+HarleyHa@users.noreply.github.com>
2023-08-03 17:56:40 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(var1, var2)   {
+    var sumTotal = var1 + var2;
+    var productTotal = var1 * var2;
+    var quotientTotal = var1 / var2;
+    var differenceTotal = var1 - var2;
+    return [sumTotal, productTotal, quotientTotal, differenceTotal];
+}
